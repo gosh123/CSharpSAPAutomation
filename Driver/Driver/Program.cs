d97c583eabb85114b7cb0f64c8ff9849a9510f9d
Neo Xie <neoanderson1987@126.com>
2015-08-04 15:29:44 +0800

    add a simple demo script

--- Driver/Driver/Program.cs
+++ Driver/Driver/Program.cs
@@ -34,20 +34,32 @@ namespace Driver
            
         static void Main(string[] args)
         {
-            string strCellValue = "";
-            SAPTestHelper.Current.SetSession();
-            //strCellValue = SAPTestHelper.Current.SAPGuiSession.FindById<GuiMainWindow>("wnd[0]").FindByName<GuiUserArea>("usr").FindByName<GuiCustomControl>("GRID1").FindByName<GuiContainerShell>("shellcont").FindByName<GuiGridView>("shell").GetCellValue(1, "From");            
-            var gridView = SAPTestHelper.Current.SAPGuiSession.FindById<GuiMainWindow>("wnd[0]").FindByName<GuiUserArea>("usr").FindByName<GuiCustomControl>("GRID1").FindByName<GuiContainerShell>("shellcont").FindByName<GuiGridView>("shell");
-            strCellValue=gridView.getCellValue(0,5);
-            gridView.SelectAll();
-            //foreach(var col in gridView.SelectedColumns)
-            //{
-            //    Console.WriteLine( gridView.GetDisplayedColumnTitle(col));
-            //    Console.WriteLine(col);
-            //}
+            //string strCellValue = "";
+            //SAPTestHelper.Current.SetSession();
+            ////strCellValue = SAPTestHelper.Current.SAPGuiSession.FindById<GuiMainWindow>("wnd[0]").FindByName<GuiUserArea>("usr").FindByName<GuiCustomControl>("GRID1").FindByName<GuiContainerShell>("shellcont").FindByName<GuiGridView>("shell").GetCellValue(1, "From");            
+            //var gridView = SAPTestHelper.Current.SAPGuiSession.FindById<GuiMainWindow>("wnd[0]").FindByName<GuiUserArea>("usr").FindByName<GuiCustomControl>("GRID1").FindByName<GuiContainerShell>("shellcont").FindByName<GuiGridView>("shell");
+            //strCellValue = gridView.getCellValue(0, 5);
+            //gridView.SelectAll();
+            ////foreach(var col in gridView.SelectedColumns)
+            ////{
+            ////    Console.WriteLine( gridView.GetDisplayedColumnTitle(col));
+            ////    Console.WriteLine(col);
+            ////}
+            ////Console.ReadLine();
+            //Console.WriteLine(strCellValue);
             //Console.ReadLine();
-            Console.WriteLine(strCellValue);
+
+
+            //SAPTestHelper.Current.SetSession();            
+            //DemoScript myScript = new DemoScript();
+            //myScript.CreateSalesOrder_Initial();
+            var myTable = SAPTestHelper.Current.SAPGuiSession.FindById<GuiMainWindow>("wnd[0]").FindByName<GuiUserArea>("usr").FindByName<GuiTabStrip>("TAXI_TABSTRIP_OVERVIEW").FindByName<GuiTab>("T\\01").FindByName<GuiScrollContainer>("SUBSCREEN_BODY:SAPMV45A:4414").FindByName<GuiSimpleContainer>("SUBSCREEN_TC:SAPMV45A:4902").FindByName<GuiGridView>("SAPMV45ATCTRL_U_ERF_GUTLAST");
+            myTable.ModifyCell(2,"Material","aabbcc");
+            string aaa = myTable.getCellValue(2, 2);
+            Console.WriteLine(aaa);
             Console.ReadLine();
+
+
         }
     }
 }
