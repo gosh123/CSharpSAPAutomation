0075d6f30e033ed659afef6ce88c3d961708e9d2
Neo Xie <neoanderson1987@126.com>
2015-08-04 17:00:25 +0800

    add table operation

--- Driver/Driver/SAPBasis.cs
+++ Driver/Driver/SAPBasis.cs
@@ -14,16 +14,42 @@ namespace Driver
 {
     public class SAPBasis
     {
+        public void StartTransction(string strTcode)
+        {
+            SAPTestHelper.Current.SAPGuiSession.StartTransaction(strTcode);            
+        }
         public void SetCurrentSession()
         {
             SAPTestHelper.Current.SetSession();
         }
-        public string SAPTable(string strTableName, int intRowIndex, string strColumnName)
+        public string SAPTable(string strTableName, int intRowIndex, int intColumnName)
         {
-            string strCellValue = "";
-            //SAPTestHelper.Current.SetSession();
-            strCellValue=SAPTestHelper.Current.SAPGuiSession.FindById<GuiMainWindow>("wnd[0]").FindByName<GuiUserArea>("usr").FindByName<GuiCustomControl>("GRID1").FindByName<GuiContainerShell>("shellcont").FindByName<GuiGridView>(strTableName).GetCellValue(intRowIndex, strColumnName);
+            string strCellValue = "";            
+            //SetCurrentSession();
+            strCellValue=SAPTestHelper.Current.SAPGuiSession.FindById<GuiMainWindow>("wnd[0]").FindByName<GuiUserArea>("usr").FindByName<GuiCustomControl>("GRID1").FindByName<GuiContainerShell>("shellcont").FindByName<GuiGridView>(strTableName).getCellValue(intRowIndex, intColumnName);
             return strCellValue;
         }
+        public void SAPTableInputValue(string strTableName, int intRowIndex, int intColumnIndex,string strInputValue)
+        {
+            SAPTestHelper.Current.SAPGuiSession.FindById<GuiMainWindow>("wnd[0]").FindByName<GuiTableControl>("SAPMV45ATCTRL_U_ERF_GUTLAST").GetCell(intRowIndex, intColumnIndex).Text = strInputValue;              
+        }
+        public void PressEnter()
+        {
+            SAPTestHelper.Current.SAPGuiSession.FindById<GuiMainWindow>("wnd[0]").SendVKey(0);
+        }
+        public void statusBarHandling()
+        {
+            int intRepeat = 0;
+            while (SAPTestHelper.Current.SAPGuiSession.FindById<GuiMainWindow>("wnd[0]").FindByName<GuiStatusbar>("sbar").Text != ""&&intRepeat<=3)
+            {
+                PressEnter();
+                intRepeat++;
+            }            
+        }
+        public void MenuBarSelect()
+        {
+            //SAPTestHelper.Current.SAPGuiSession.FindById<GuiMainWindow>("wnd[0]").FindByName<GuiMenubar>("mbar").
+        }
+
     }
 }
